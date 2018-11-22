98df006a43019fe6044817adfc8930ac86024d3b
Jonas Hecht <jonas.hecht@codecentric.de>
2018-11-22 18:13:45 +0100

    Fixing Test test_is_docker_installed - wrong package name corrected

--- docker/tests/test_default.py
+++ docker/tests/test_default.py
@@ -4,11 +4,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
 def test_is_docker_installed(host):
-    package_docker = host.package("docker")
+    package_docker = host.package('docker-ce')
+
     assert package_docker.is_installed
 
 
 def test_vagrant_user_is_part_of_group_docker(host):
-    user_vagrant = host.user("vagrant")
+    user_vagrant = host.user('vagrant')
+
     assert user_vagrant.group == 'docker'
